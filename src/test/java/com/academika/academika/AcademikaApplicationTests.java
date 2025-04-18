811d4747c7f434005dc21e0adced765c584338ad
Jhonatan <jhonatanhuaman76@gmail.com>
2025-04-18 08:30:46 -0500

    Primer commit

---
+++ src/test/java/com/academika/academika/AcademikaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academika.academika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademikaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
